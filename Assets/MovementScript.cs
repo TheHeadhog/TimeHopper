c71f47517396aa4dab944b7e1f4b313259e53097
Headhog <stevan.popovic.headhog@gmail.com>
2020-11-09 23:50:34 +0100

    Prvi komit

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed = 0.7f;
+    public float jumpForce = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float dirHorizontal = Input.GetAxis("Horizontal");
+        rb2d.AddForce(new Vector2(dirHorizontal, 0) * speed,ForceMode2D.Impulse);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(new Vector2(0, 1) * jumpForce, ForceMode2D.Impulse);
+        }
+        
+    }
+}
---
+++ Assets/PlayerStatus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public int Health;
+    private int Fuel;
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        takeDamage();
+    }
+
+    public void takeDamage(int dmg=1)
+    {
+        Health -= dmg;
+        if (Health <= 0)
+        {
+            (gameObject.GetComponent<Rigidbody2D>()).simulated=false;
+        }
+    }
+}
